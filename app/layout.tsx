6899c087312eead1a003db901ac88ab850d75b0e
Ye Yu <nearjilt@gmail.com>
2025-02-08 14:53:05 +0800

    chore: add prettierrc and test basic routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import "~/styles/tailwind.css";
+import { Open_Sans } from "next/font/google";
+
+const OpenSansFont = Open_Sans({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={OpenSansFont.className}>
       <body>{children}</body>
     </html>
   );
