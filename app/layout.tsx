caebe3b66a8c93b26e6dd4bd7f2bcaa18220e092
Ye Yu <nearjilt@gmail.com>
2025-02-20 20:57:12 +0800

    refactor: add stars background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import '~/styles/tailwind.css'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { shadesOfPurple } from '@clerk/themes'
 import { ThemeProvider } from '~/components/providers/theme-provider'
 import { ModalProvider } from '~/components/providers/modal-provider'
 import { SocketProvider } from '~/components/providers/socket-provider'
 import { QueryProvider } from '~/components/providers/query-provider'
+import StarsBackground from '~/components/layout/stars-background.tsx'
 
 const OpenSansFont = Open_Sans({
   subsets: ['latin'],
@@ -16,14 +18,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    // * 注意包裹的是 html 喵~
-    <ClerkProvider afterSignOutUrl={'/'}>
+    <ClerkProvider
+      afterSignOutUrl={'/'}
+      appearance={{
+        baseTheme: shadesOfPurple,
+      }}
+    >
       <html
         lang="zh-CN"
         className={OpenSansFont.className}
         suppressHydrationWarning
       >
-        <body className="dark:bg-slate-700 bg-slate-300">
+        <body className="bg-black">
           <ThemeProvider
             attribute={'class'}
             defaultTheme="dark"
@@ -35,6 +41,7 @@ export default function RootLayout({
               <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
+          <StarsBackground />
         </body>
       </html>
     </ClerkProvider>
